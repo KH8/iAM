a5952b90e5ca830a08fb709daa019922d58d1c0d
Krzysztof Reczek <reczek.krzysztof@gmail.com>
2015-01-06 19:46:46 +0100

    Initial Commit

---
+++ iAM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iAM
+//
+//  Created by Krzysztof Reczek on 06.01.2015.
+//  Copyright (c) 2015 H@E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

c6059f0ecd67d681f12b9fd50247b4a253e140bc
Krzysztof Reczek <reczek.krzysztof@gmail.com>
2015-01-20 20:33:35 +0100

    logger

---
+++ iAM/Auxiliaries/AMLogger.h
@@ -0,0 +1,10 @@
+//
+// Created by Krzysztof Reczek on 20.01.15.
+// Copyright (c) 2015 H@E. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AMLogger : NSObject
+@end
---
+++ iAM/Auxiliaries/AMLogger.m
@@ -0,0 +1,12 @@
+//
+// Created by Krzysztof Reczek on 20.01.15.
+// Copyright (c) 2015 H@E. All rights reserved.
+//
+
+#import "AMLogger.h"
+
+
+@implementation AMLogger {
+
+}
+@end

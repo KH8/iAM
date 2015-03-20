f007e926fdac099938006b38dd54ec69eb753d40
Krzysztof Reczek <reczek.krzysztof@gmail.com>
2015-03-20 18:41:29 +0100

    AMMutableArray init

---
+++ iAM/Core/Auxiliaries/AMMutableArray.h
@@ -0,0 +1,38 @@
+//
+//  AMMutableArray.h
+//  iAM
+//
+//  Created by Krzysztof Reczek on 20.03.2015.
+//  Copyright (c) 2015 H@E. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol AMMutableArrayDelegate <NSObject>
+
+@required
+
+- (void)arrayHasBeenChanged;
+- (void)selectionHasBeenChanged;
+
+@end
+
+@interface AMMutableArray : NSMutableArray
+
+@property (nonatomic, weak) id <AMMutableArrayDelegate> delegate;
+
+- (id)init;
+
+- (void)addObject:(NSObject*)newObject;
+- (void)addObject:(NSObject*)newObject atIndex:(NSUInteger)anIndex;
+
+- (void)removeActualObject;
+- (void)removeObjectAtIndex:(NSUInteger)anIndex;
+
+- (NSInteger)getActualIndex;
+- (NSObject*)getActualObject;
+- (NSObject*)getObjectAtIndex: (NSUInteger)anIndex;
+
+- (void)setIndexAsActual:(NSUInteger)anIndex;
+
+@end
---
+++ iAM/Core/Auxiliaries/AMMutableArray.m
@@ -0,0 +1,52 @@
+//
+//  AMMutableArray.m
+//  iAM
+//
+//  Created by Krzysztof Reczek on 20.03.2015.
+//  Copyright (c) 2015 H@E. All rights reserved.
+//
+
+#import "AMMutableArray.h"
+
+@implementation AMMutableArray
+
+- (id)init{
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)addObject:(NSObject*)newObject{
+    
+}
+
+- (void)addObject:(NSObject*)newObject atIndex:(NSUInteger)anIndex{
+    
+}
+
+- (void)removeActualObject{
+    
+}
+
+- (void)removeObjectAtIndex:(NSUInteger)anIndex{
+    
+}
+
+- (NSInteger)getActualIndex{
+    return 0;
+}
+
+- (NSObject*)getActualObject{
+    return 0;
+}
+
+- (NSObject*)getObjectAtIndex: (NSUInteger)anIndex{
+    return 0;
+}
+
+- (void)setIndexAsActual:(NSUInteger)anIndex{
+    
+}
+
+@end

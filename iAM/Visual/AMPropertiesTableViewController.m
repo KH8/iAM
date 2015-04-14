40e37aa982f64b4aaac58855436bc930cd78d865
Krzysztof Reczek <reczek.krzysztof@gmail.com>
2015-04-14 19:19:15 +0200

    volume sliders

--- iAM/Visual/AMPropertiesTableViewController.m
+++ iAM/Visual/AMPropertiesTableViewController.m
@@ -10,12 +10,22 @@
 #import "AMPropertiesTableViewController.h"
 #import "AMSoundsTableViewController.h"
 #import "AMSequencerSingleton.h"
+#import "AMVolumeSlider.h"
 
 @interface AMPropertiesTableViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *track1SoundLabel;
 @property (weak, nonatomic) IBOutlet UILabel *track2SoundLabel;
 @property (weak, nonatomic) IBOutlet UILabel *track3SoundLabel;
+@property (weak, nonatomic) IBOutlet AMVolumeSlider *generalSlider;
+@property (weak, nonatomic) IBOutlet AMVolumeSlider *track1Slider;
+@property (weak, nonatomic) IBOutlet AMVolumeSlider *track2Slider;
+@property (weak, nonatomic) IBOutlet AMVolumeSlider *track3Slider;
+
+@property NSTimer *mainTimer;
+@property NSRunLoop *runner;
+@property BOOL playSound;
+@property AMPlayer *player;
 
 @property NSArray *arrayOfPlayers;
 
@@ -28,11 +38,13 @@
     [self loadSidebarMenu];
     [self loadIcons];
     [self loadMainObjects];
+    [self loadPlayBack];
 }
 
 -(void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
     [self loadLabels];
+    [self loadSliders];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -68,10 +80,28 @@
     _arrayOfPlayers = sequencer.getArrayOfPlayers;
 }
 
+- (void)loadPlayBack{
+    _playSound = NO;
+    _mainTimer = [NSTimer scheduledTimerWithTimeInterval:0.5
+                                                  target:self
+                                                selector:@selector(onTick)
+                                                userInfo:nil
+                                                 repeats:YES];
+    _runner = [NSRunLoop currentRunLoop];
+    [_runner addTimer:_mainTimer forMode: NSRunLoopCommonModes];
+}
+
 - (void)loadLabels{
     _track1SoundLabel.text = [(AMPlayer*)_arrayOfPlayers[0] getSoundKey];
     _track2SoundLabel.text = [(AMPlayer*)_arrayOfPlayers[1] getSoundKey];
     _track3SoundLabel.text = [(AMPlayer*)_arrayOfPlayers[2] getSoundKey];
+}
+
+- (void)loadSliders{
+    _generalSlider.value = [[(AMPlayer*)_arrayOfPlayers[0] getGeneralVolumeFactor] floatValue];
+    _track1Slider.value = [[(AMPlayer*)_arrayOfPlayers[0] getVolumeFactor] floatValue];
+    _track2Slider.value = [[(AMPlayer*)_arrayOfPlayers[1] getVolumeFactor] floatValue];
+    _track3Slider.value = [[(AMPlayer*)_arrayOfPlayers[2] getVolumeFactor] floatValue];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
@@ -90,6 +120,42 @@
     if ([[segue identifier] isEqualToString:@"sw_track3"])
     {
         [rootController assignPlayer:_arrayOfPlayers[2]];
+    }
+}
+
+- (IBAction)generalTrackVolumeChanged:(id)sender {
+    [(AMPlayer*)_arrayOfPlayers[0] setGeneralVolumeFactor:[[NSNumber alloc] initWithFloat:_generalSlider.value]];
+    [(AMPlayer*)_arrayOfPlayers[1] setGeneralVolumeFactor:[[NSNumber alloc] initWithFloat:_generalSlider.value]];
+    [(AMPlayer*)_arrayOfPlayers[2] setGeneralVolumeFactor:[[NSNumber alloc] initWithFloat:_generalSlider.value]];
+    _player = (AMPlayer*)_arrayOfPlayers[0];
+    _playSound = YES;
+}
+
+- (IBAction)track1VolumeChanged:(id)sender {
+    [self trackVolumeChanged:(AMPlayer*)_arrayOfPlayers[0]
+                   withValue:_track1Slider.value];
+}
+
+- (IBAction)track2VolumeChanged:(id)sender {
+    [self trackVolumeChanged:(AMPlayer*)_arrayOfPlayers[1]
+                   withValue:_track2Slider.value];
+}
+
+- (IBAction)track3VolumeChanged:(id)sender {
+    [self trackVolumeChanged:(AMPlayer*)_arrayOfPlayers[2]
+                   withValue:_track3Slider.value];
+}
+
+- (void)trackVolumeChanged:(AMPlayer*)player withValue:(float)volume{
+    [player setVolumeFactor:[[NSNumber alloc] initWithFloat:volume]];
+    _player = player;
+    _playSound = YES;
+}
+
+- (void)onTick {
+    if(_playSound){
+        _playSound = NO;
+        [_player playSound];
     }
 }
 

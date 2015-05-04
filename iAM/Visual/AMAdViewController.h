b0c4181bd91c1968d4d756c4f9c07b7e222ae1b1
Krzysztof Reczek <reczek.krzysztof@gmail.com>
2015-05-04 20:10:39 +0200

    iad ...

---
+++ iAM/Visual/AMAdViewController.h
@@ -0,0 +1,13 @@
+//
+//  AMAdViewController.h
+//  iAM
+//
+//  Created by Krzysztof Reczek on 04.05.2015.
+//  Copyright (c) 2015 miscApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMAdViewController : UIViewController
+
+@end
